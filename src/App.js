75011ce62b8750ed5bb004b2d50821d1db46c498
daniel0809 <daniel08099@gmail.com>
2024-02-22 11:41:53 -0800

    api

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const BlockchainStats = () => {
     // transactions: 967306183,
     // addresses: 1206456923,
     unspentOutputs: 216828930,
-    blockchainSize: 512.91,
+    // blockchainSize: 512.91,
     bitcoinCirculatingSupply: 19632283.54497096,
     distribution: {
       '1000000-100000': 4,
@@ -23,36 +23,63 @@ const BlockchainStats = () => {
   const [blocks, setBlocks] = useState(0);
   const [transactions, setTransactions] = useState(0);
   const [addresses, setAddress] = useState(0);
+  const [blockchainSize, setBlockchainSize] = useState(0);
   let data = JSON.stringify({
-    "query": "query MyQuery {\n  bitcoin(network: bitcoin) {\n    transactions(inputCount: {gt: 0}) {\n      count\n    }\n    blocks {\n      count\n    }\n    inputs {\n      count(uniq: addresses)\n    }\n  }\n}\n",
+    "query": "query MyQuery {\n  bitcoin(network: bitcoin) {\n    transactions(any: {txSize: {gt: 0}}) {\n      count\n    }\n    inputs {\n      count(uniq: addresses)\n    }\n  }\n}\n",
     "variables": "{}"
  });
-
-
-  let config = {
+ 
+ let config = {
     method: 'post',
     maxBodyLength: Infinity,
     url: 'https://graphql.bitquery.io',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-KEY': 'BQYJiKjTyOSZTJZgYOhXFHggkNP5FQDc',
-      'Authorization': 'Bearer ory_at_atL_ckHbpjH3kS440_psx4lOJOj0fOkwVxIvuPVAsig.9lbSKfbESdwJwbGR21A9oNm229NRzTvCHW1LqNrNbMA'
+    headers: { 
+       'Content-Type': 'application/json', 
+       'X-API-KEY': 'BQYNjr1N51m2Qp2lNPBM8vQVbYqWtEtV', 
+       'Authorization': 'Bearer ory_at_xMjeK6fixvaBov5B0Fez112mtDOortVT0F6jXToITHM.Tiu35jz-epXCfGtIUC49IoFi0nUdymnMsTglB6_-jLE'
     },
-    data: data
+    data : data
+ };
+  let config_2 = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://api.blockchain.info/stats',
+    headers: {
+    },
+    // data: data
   };
 
-  axios.request(config)
+  axios.request(config_2)
     .then((response) => {
       console.log(JSON.stringify(response.data));
-      setTransactions(response.data.data.bitcoin.transactions[0].count)
-      setBlocks(response.data.data.bitcoin.blocks[0].count)
-      setAddress(response.data.data.bitcoin.inputs[0].count)
+      // setTransactions(response.data.data.bitcoin.transactions[0].count)
+      // setBlocks(response.data.data.bitcoin.blocks[0].count)
+      // setAddress(response.data.data.bitcoin.inputs[0].count)
+      // setTransactions(response.data.data.bitcoin.transactions[0].count)
+      setBlocks(response.data.n_blocks_total)
+      // setAddress(response.data.data.bitcoin.inputs[0].count)
+      setBlockchainSize(response.data["blocks_size"])
     })
     .catch((error) => {
       console.log(error);
     });
 
-    
+  axios.request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      setTransactions(response.data.data.bitcoin.transactions[0].count)
+      // setBlocks(response.data.data.bitcoin.blocks[0].count)
+      setAddress(response.data.data.bitcoin.inputs[0].count)
+      // setTransactions(response.data.data.bitcoin.transactions[0].count)
+      // setBlocks(response.data.n_blocks_total)
+      setAddress(response.data.data.bitcoin.inputs[0].count)
+      // setBlockchainSize(response.data["blocks_size"])
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+
   return (
     <div className="blockchain-stats">
       <h1>Blockchain State</h1>
@@ -75,7 +102,7 @@ const BlockchainStats = () => {
         </div>
         <div className="stat">
           <h2>Blockchain size</h2>
-          <p>{stats.blockchainSize}</p>
+          <p>{blockchainSize}</p>
         </div>
         <div className="stat">
           <h2>Bitcoin circulating supply</h2>
